d030bf9ffac20f7104a67a2745c03b80f9fffd81
Edilson3012 <edilsongotardi@live.com>
2019-04-23 20:29:36 -0300

    Classe para criar objeto do tipo Conta

---
+++ workspace/estudosCassiano/2019-04-23/encapsulamento/Movimentação.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package encapsulamento;
+
+/**
+ * @descrition Classe criada para testar a conta corrente do cliente
+ *
+ * @author Edilson Gotardi Murbach
+ *
+ * @since Classe criada no dia 23 de abr de 2019 as 20:24:06
+ *
+ * @version 
+ *
+ */
+public class Movimentação {
+	
+	public static void main(String java[]) {
+		
+		Conta c = new Conta("Bruna", 36275, 25000, "123");
+		
+		c.exibirDadosContaCliente();
+		
+	}
+	
+	
+}
