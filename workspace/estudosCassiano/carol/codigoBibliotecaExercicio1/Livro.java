7739a4a3893cb573253dea3bf8da67884effb6db
Edilson3012 <edilsongotardi@live.com>
2019-05-09 22:20:32 -0300

    Adicionando classes da Carol

---
+++ workspace/estudosCassiano/carol/codigoBibliotecaExercicio1/Livro.java
@@ -0,0 +1,15 @@
+package codigoBibliotecaExercicio1;
+
+public class Livro {
+	Titulo titulo;
+
+	public Livro(int codigo) {
+		// instância um titulo e o associa ao livro
+		titulo = new Titulo(codigo);
+	}
+
+	public int verPrazo() {
+		return titulo.getPrazo();
+	}
+
+}
