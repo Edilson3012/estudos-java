ce638e176fb05e9b8f0e785884a9697b91183df5
Edilson3012 <edilsongotardi@live.com>
2019-05-14 19:50:15 -0300

    Classe para demonstrar tratamento de excessão

---
+++ workspace/estudosCassiano/2019-05-14/excessao/Divisao.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package excessao;
+
+/**
+ * @descrition Classe criada para demonstrar a utilização do tratamento de exceção feito com try..catch...throws
+ *
+ * @author Edilson Gotardi Murbach
+ *
+ * @since Classe criada no dia 14 de mai de 2019 as 19:46:59
+ *
+ * @version 
+ *
+ */
+public class Divisao {
+	
+	public static void main(String x[]) {
+		
+		int a, b, div;
+		a = b = div = 0;
+		
+		a = 8;
+		b = 2;
+		div = a/b;
+		System.out.println("Resultado: "+div);
+	}
+	
+}
