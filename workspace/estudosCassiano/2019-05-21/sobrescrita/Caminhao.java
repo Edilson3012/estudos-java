43fe27dad17e3eb0a4b83053310633f7d22532e4
Edilson3012 <edilsongotardi@live.com>
2019-05-21 21:33:37 -0300

    Classes para testar sobrescrita

---
+++ workspace/estudosCassiano/2019-05-21/sobrescrita/Caminhao.java
@@ -0,0 +1,83 @@
+/**
+ * 
+ */
+package sobrescrita;
+
+/**
+ * @descrition Classe criada para ...
+ *
+ * @author Edilson Gotardi Murbach
+ *
+ * @since Classe criada no dia 21 de mai de 2019 as 20:36:55
+ *
+ * @version 
+ *
+ */
+public class Caminhao extends Veiculo{
+	
+	String modelo;
+	float peso;
+	boolean trucado;
+	
+	public void exibirDados() {
+		System.out.println("É um caminhão do modelo "+getModelo()+""
+				+ " de "+getPeso() + ""
+				+ " toneladas "+ ""
+				+ "do tipo trucado "+(isTrucado()? "Sim": "não"));		
+	}
+	
+	/**
+	 * @param placa
+	 * @param marca
+	 * @param anoFab
+	 * @param modelo
+	 * @param peso
+	 * @param trucado
+	 */
+	public Caminhao(String placa, String marca, int anoFab, String modelo, float peso, boolean trucado) {
+		super(placa, marca, anoFab);
+		this.modelo = modelo;
+		this.peso = peso;
+		this.trucado = trucado;
+	}
+
+
+
+
+	/**
+	 * @return the modelo
+	 */
+	public String getModelo() {
+		return modelo;
+	}
+	/**
+	 * @param modelo the modelo to set
+	 */
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	/**
+	 * @return the peso
+	 */
+	public float getPeso() {
+		return peso;
+	}
+	/**
+	 * @param peso the peso to set
+	 */
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+	/**
+	 * @return the trucado
+	 */
+	public boolean isTrucado() {
+		return trucado;
+	}
+	/**
+	 * @param trucado the trucado to set
+	 */
+	public void setTrucado(boolean trucado) {
+		this.trucado = trucado;
+	}
+}
---
+++ workspace/estudosCassiano/2019-05-21/sobrescrita/Concessionaria.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package sobrescrita;
+
+/**
+ * @descrition Classe criada para instanciar objetos do tipo Veiculo
+ *
+ * @author Edilson Gotardi Murbach
+ *
+ * @since Classe criada no dia 21 de mai de 2019 as 20:31:25
+ *
+ * @version 
+ *
+ */
+public class Concessionaria {
+	
+	public static void main(String ipisulon[]) {
+		Veiculo v = new Veiculo("BSN-4908", "Honda", 1999);
+		v.exibirDados();
+		
+		System.out.println("-----------------------------\n\n");
+		
+		Carro c = new Carro("HRR-4060", "GM", 2016, "Turbo", 98750f);
+		c.exibirDados();
+	}
+	
+}
