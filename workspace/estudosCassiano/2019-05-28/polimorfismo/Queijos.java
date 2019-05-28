d0a76a364fbc2728af67b5c68387317d9c8325b8
Edilson3012 <edilsongotardi@live.com>
2019-05-28 20:58:31 -0300

    Classe que implementa todos os métodos da interface PIZZA

---
+++ workspace/estudosCassiano/2019-05-28/polimorfismo/Queijos.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package polimorfismo;
+
+/**
+ * @descrition Classe que implementa TODOS os métodos da interface Pizza
+ *
+ * @author Edilson Gotardi Murbach
+ *
+ * @since Classe criada no dia 28 de mai de 2019 as 20:56:47
+ *
+ * @version 
+ *
+ */
+public class Queijos implements Pizza {
+
+	/* 
+	 * @see polimorfismo.Pizza#montar()
+	 */
+	@Override
+	public void montar() {
+		System.out.println("Mussarela, catupiri, chedar, provolone");
+	}
+
+	/* 
+	 * @see polimorfismo.Pizza#assar()
+	 */
+	@Override
+	public void assar() {
+		System.out.println("Tempo de forno: 10 minutos");
+	}
+
+	/* 
+	 * @see polimorfismo.Pizza#cobrar()
+	 */
+	@Override
+	public void cobrar() {
+		System.out.println("Preço: R$ 39,90");
+	}
+
+}
